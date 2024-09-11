c0b7b81fac8ca35d7c3dd0ce3792f6b06ee70b2f
Lívia Kelebercová <37534023+livi83@users.noreply.github.com>
2024-09-11 23:12:58 +0200

    manifest and init files

---
+++ __manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Estate',
+    'version': '1.0',
+    'summary': 'An estate modul',
+    'description': 'Description',
+    'author': 'Lívia Kelebercová',
+    'depends': ['base'],
+    'data': [],
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
+}
