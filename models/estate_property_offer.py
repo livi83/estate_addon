48a661f08232aca1f59e01ca72da767ee4a88e85
Lívia Kelebercová <37534023+livi83@users.noreply.github.com>
2024-09-12 19:45:16 +0200

    Accept Refuse (buttons not working)

--- models/estate_property_offer.py
+++ models/estate_property_offer.py
@@ -1,4 +1,5 @@
 from odoo import models, fields
+from odoo.exceptions import UserError
 
 class EstatePropertyOffer(models.Model):
     _name = 'estate.property.offer'
@@ -11,3 +12,20 @@ class EstatePropertyOffer(models.Model):
     ], string='Status', default='refused')
     partner_id = fields.Many2one('res.partner', string='Partner', required=True)
     property_id = fields.Many2one('estate.property', string='Property', required=True, ondelete='cascade')
+
+    def action_accept(self):
+        """Accept the offer."""
+        if self.property_id.state == 'sold':
+            raise UserError("Cannot accept offers for sold properties.")
+        if self.property_id.offer_ids.filtered(lambda offer: offer.state == 'accepted'):
+            raise UserError("Only one offer can be accepted for this property.")
+        self.status = 'accepted'
+        self.property_id.buyer_id = self.partner_id
+        self.property_id.selling_price = self.price
+        self.property_id.state = 'offer_accepted'
+
+    def action_refuse(self):
+        """Refuse the offer."""
+        if self.status == 'accepted':
+            raise UserError("Cannot refuse an accepted offer.")
+        self.state = 'refused'
