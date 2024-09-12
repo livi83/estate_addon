efccfdba065cb370e75fb17aa47ae418d8e28b75
Lívia Kelebercová <37534023+livi83@users.noreply.github.com>
2024-09-12 09:27:27 +0200

    estate_property migration

---
+++ models/estate_property.py
@@ -0,0 +1,32 @@
+from odoo import models, fields
+
+class EstateProperty(models.Model):
+    _name = "estate.property"
+    _description = "Real Estate Property"
+
+    # Set the 'name' and 'expected_price' fields as required (not nullable in the database)
+    name = fields.Char(string="Title", required=True)
+    description = fields.Text(string="Description")
+    postcode = fields.Char(string="Postcode")
+    date_availability = fields.Date(string="Available From")
+    
+    # Required field
+    expected_price = fields.Float(string="Expected Price", required=True)
+    
+    selling_price = fields.Float(string="Selling Price", readonly=True)
+    bedrooms = fields.Integer(string="Bedrooms")
+    living_area = fields.Integer(string="Living Area (sqm)")
+    facades = fields.Integer(string="Facades")
+    garage = fields.Boolean(string="Garage")
+    garden = fields.Boolean(string="Garden")
+    garden_area = fields.Integer(string="Garden Area (sqm)")
+    garden_orientation = fields.Selection(
+        [('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')],
+        string="Garden Orientation"
+    )
+
+    # Default fields for tracking creation and updates
+    create_uid = fields.Many2one('res.users', string="Created by", readonly=True)
+    create_date = fields.Datetime(string="Created on", readonly=True)
+    write_uid = fields.Many2one('res.users', string="Last Updated by", readonly=True)
+    write_date = fields.Datetime(string="Last Updated on", readonly=True)
