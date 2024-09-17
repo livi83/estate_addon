b2dbad8e23c8771e8b66b2c669942c73f09ab07f
Lívia Kelebercová <37534023+livi83@users.noreply.github.com>
2024-09-17 22:49:48 +0200

    Unique tag names and property types

--- models/estate_property_tag.py
+++ models/estate_property_tag.py
@@ -1,7 +1,18 @@
-from odoo import models, fields
+from odoo import models, fields, api
+from odoo.exceptions import UserError, ValidationError
 
 class EstatePropertyTag(models.Model):
     _name = "estate.property.tag"
     _description = "Real Estate Property Tag"
 
     name = fields.Char(string="Tag Name", required=True)
+
+    @api.constrains('name')
+    def _check_unique_name(self):
+        for record in self:
+            existing_tag = self.search([
+                ('name', '=', record.name),
+                ('id', '!=', record.id)  # Exclude current record from the search
+            ])
+            if existing_tag:
+                raise ValidationError("The tag name must be unique.")
--- models/estate_property_type.py
+++ models/estate_property_type.py
@@ -1,7 +1,18 @@
-from odoo import models, fields
+from odoo import models, fields, api
+from odoo.exceptions import UserError, ValidationError
 
 class EstatePropertyType(models.Model):
     _name = 'estate.property.type'
     _description = 'Real Estate Property Type'
 
     name = fields.Char(required=True, string='Name')
+
+    @api.constrains('name')
+    def _check_unique_name(self):
+        for record in self:
+            existing_tag = self.search([
+                ('name', '=', record.name),
+                ('id', '!=', record.id)  # Exclude current record from the search
+            ])
+            if existing_tag:
+                raise ValidationError("The property type must be unique.")
